6970e4f8cb0dbc2d3254c8dadee1752b5b02b8cb
VIRALKUMAR PATEL <bindup084@gmail.com>
2020-12-03 17:47:10 -0500

    COIN-OBJECT

--- Coin_Object/main.js
+++ Coin_Object/main.js
@@ -1,63 +1,49 @@
 let coin = {
   state: 0,
   flip: function () {
-    let headsOrTails = calculateRandomOneOrZero();
-    this.state = headsOrTails;
-
-    /* 1. Randomly set your coin object's "state" property to be either 
-           0 or 1: use "this.state" to access the "state" property on this object.*/
+    this.state = Math.round(Math.random());
   },
+
   toString: function () {
-    if (this.state === 1) {
+    if (this.state === 0) {
       return "Heads";
     } else {
       return "Tails";
     }
-
-    /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
   },
+
   toHTML: function () {
     let images = document.createElement("img");
+
     images.style.width = "100px";
     images.style.height = "100px";
-    images.src = "./images/Heads.jpg/Tails.jpg";
-    images.append();
-    document.body.append(images);
-    let ilist = document.images;
 
-    for (let i = 0; i < ilist.length; i++) {
-      if (ilist[i].src == "Heads.jpg") {
-        // found the Head
-      }
+    if (this.state === 0) {
+      images.src = "images/Heads.jpg";
+      images.alt = "Quarter Heads";
+    } else {
+      images.src = "images/Tails.jpg";
     }
-    /* 3. Set the properties of this image element to show either face-up
-           or face-down, depending on whether this.state is 0 or 1.*/
     return images;
   },
 };
 
 function display20Flips() {}
-for (let i = 1; i <= 20; i++) {
-  let numDiv = document.createElement("div");
-  numDiv.append(i);
-  document.body.append(numDiv);
+for (let i = 0; i < 20; i++) {
+  coin.flip();
+  coin.toString();
+  document.body.append(coin.toString());
+  let li = document.createElement("li");
+  li.innerHTML = coin.toString();
+  document.body.append(li);
 }
 
 function display20Images() {
-  for (let j = 0; j <= 20; j++) {
-    let numDiv1 = document.createElement("div");
-    numDiv1.append(j);
-    document.body.append(numDiv1);
+  for (let i = 0; i < 20; i++) {
+    coin.flip();
+    document.body.append(coin.toHTML());
   }
 }
-for (let Heads = 1; Heads <= 20; Heads++) {
-  let images = document.createElement("img");
-  images.append(Heads);
-  document.body.append(images);
-}
-// for (let i = 1; i <= 20; i++) {
-//   let numDiv = document.createElement("div");
-//   numDiv.append(i);
-//   document.body.append(numDiv);
-// }
+
+display20Flips();
+display20Images();
